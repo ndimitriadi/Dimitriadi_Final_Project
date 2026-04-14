2d3e8ce11f9b965f3bf8a19dfd15057acfe374e9
ndimitriadi <nikoletta.dimitriadi@acg.edu>
2026-04-14 19:45:49 +0300

    added About us and Contact us page

--- athenian_nectar/home/urls.py
+++ athenian_nectar/home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+
+    path('about/', views.about, name='about'),
+
+    path('contact/', views.contact, name='contact'),
 ]
--- athenian_nectar/home/views.py
+++ athenian_nectar/home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about_us.html')
+
+def contact(request):
+    return render(request, 'home/contact_us.html')
