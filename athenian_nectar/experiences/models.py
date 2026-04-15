ce37cef154dc8736cc210567bf0956ca44d1f866
ndimitriadi <nikoletta.dimitriadi@acg.edu>
2026-04-16 01:07:58 +0300

    added more experiences

--- athenian_nectar/experiences/models.py
+++ athenian_nectar/experiences/models.py
@@ -12,9 +12,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Subcategories"
+
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
+
 class Experience(models.Model):
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, blank=True, null=True)    
     description = models.TextField()
     image = models.ImageField(upload_to='experiences/')
     price = models.DecimalField(max_digits=6, decimal_places=2)
