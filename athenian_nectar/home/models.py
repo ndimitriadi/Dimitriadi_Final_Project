0692ddf9e9c8e85b2a1aba4657fd8826ffb7f60d
ndimitriadi <nikoletta.dimitriadi@acg.edu>
2026-04-15 15:59:22 +0300

    improved testimonials using django features

--- athenian_nectar/home/models.py
+++ athenian_nectar/home/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
 class Testimonial(models.Model):
+
+    star_options = [
+        (1, '1 Star - ★☆☆☆☆'),
+        (2, '2 Stars - ★★☆☆☆'),
+        (3, '3 Stars - ★★★☆☆'),
+        (4, '4 Stars - ★★★★☆'),
+        (5, '5 Stars - ★★★★★'),
+    ]
+
     name = models.CharField(max_length=100)
     role = models.CharField(max_length=100)
     quote = models.TextField()
-    stars = models.IntegerField(default=5)
+    stars = models.IntegerField(default=5, choices=star_options)
 
     def __str__(self):
         return self.name
