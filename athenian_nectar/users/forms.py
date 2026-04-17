753edbb234cf3cff0d7d3001666241eb21fb2159
ndimitriadi <nikoletta.dimitriadi@acg.edu>
2026-04-17 13:36:33 +0300

    added register page and fixed error messages

---
+++ athenian_nectar/users/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=20, 
+        min_length=4, 
+    )
+
+    email = forms.EmailField(
+        max_length=35, 
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email'] 
+
+    #ensures lowercase usernames
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        return username.lower()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields.values():
+            field.help_text = ''
+
+        for field_name, field in self.fields.items():
+            if 'password' in field_name.lower():
+                field.widget.attrs['maxlength'] = '30'
--- athenian_nectar/users/views.py
+++ athenian_nectar/users/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from .forms import CustomRegisterForm
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('dashboard')
     else:
-        form = UserCreationForm()
+        form = CustomRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 @login_required
