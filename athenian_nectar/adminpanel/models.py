35c297344dc484c0fbc435ea4f45d358abc3451d
ndimitriadi <nikoletta.dimitriadi@acg.edu>
2026-04-24 16:15:40 +0300

    fixed small stuff

--- athenian_nectar/adminpanel/models.py
+++ athenian_nectar/adminpanel/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Experience(models.Model):
-    title = models.CharField(max_length=50) 
+    title = models.CharField(max_length=50, unique=True) 
     description = models.TextField()
 
 class Category(models.Model):
-    name = models.CharField(max_length=20) 
+    name = models.CharField(max_length=20, unique=True) 
 
